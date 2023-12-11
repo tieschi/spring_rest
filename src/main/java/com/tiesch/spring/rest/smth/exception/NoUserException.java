0656c60ff0f3c71152953041eec3d34c6012c15a
tieschi <tiesch@bk.ru>
2023-12-11 08:34:44 +0300

    html-файлы перенеси в папку view, в package smth не должно быть вьюшек;
    РестКонтроллер в этой задаче не нужно создавать;
    
    В папке resources создай файл db.properties (отсутствует папка ресурсы и файл ресурсов),  в конфиг тяни значение из этого файла с помощью @PropertySource, данные для подключения к БД и настройки хибернейта хранить в классе Java -  не самая лучшая идея;

--- src/main/java/com/tiesch/spring/rest/smth/exception/NoUserException.java
+++
@@ -1,8 +0,0 @@
-package com.tiesch.spring.rest.smth.exception;
-
-public class NoUserException extends RuntimeException{
-
-    public NoUserException(String message) {
-        super(message);
-    }
-}
